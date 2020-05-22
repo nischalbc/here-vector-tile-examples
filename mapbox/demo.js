2c7342f00553c330e39b65f992468a50fc88fe4e
Nischal Kumar <nischal@trinetraconsulting.com>
2020-05-22 12:36:37 +0530

    Update demo.js

--- mapbox/demo.js
+++ mapbox/demo.js
@@ -1,12 +1,12 @@
-function initializeMap() {
-   new mapboxgl.Map({
+window.addevent('domready', function(){
+   
+      new mapboxgl.Map({
       container: 'map',
       style: 'https://assets.vector.hereapi.com/styles/berlin/base/mapbox/tilezen?apikey=' + apikey,
       center: [-122.431297, 37.773972], //Longitude, Latitude
       zoom: 11
    });
-}
+   
+});
 
-onload = function () {
-   initializeMap();
-}
+
